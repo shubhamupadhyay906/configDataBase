5f15aaba73087a808c0ea484e85fdf5d721d5104
shubh <shubham@gmail.com>
2023-09-24 20:21:37 +0530

    config

---
+++ src/main/java/com/example/multpledbs/MultpledbsApplication.java
@@ -0,0 +1,13 @@
+package com.example.multpledbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultpledbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultpledbsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/multpledbs/mysql/mysql.java
@@ -0,0 +1,4 @@
+package com.example.multpledbs.mysql;
+
+public class mysql {
+}
